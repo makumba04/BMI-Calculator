3607c81d0cc4b6b2226aacbc73578d58d3dc86a6
Pablo Cabello Ruz <127546689+makumba04@users.noreply.github.com>
2024-08-13 19:26:04 +0200

    Finished app - Looking for any bug...

--- public/main.js
+++ public/main.js
@@ -1,29 +1,83 @@
 function BMI_METRIC() {
 
-    var weight = parseFloat(document.getElementById('INPUT_WEIGHT').value);
-    var height = parseFloat(document.getElementById('INPUT_HEIGHT').value);
-    var result_container = document.getElementById('result');
+    var weight = parseFloat(document.getElementById('INPUT_WEIGHT_METRIC').value);
+    var height = parseFloat(document.getElementById('INPUT_HEIGHT_METRIC').value);
+    var result_container = document.getElementById('RESULT_METRIC');
+    result_container.innerHTML = '';
 
-    var BMI_VALUE = (weight / (height**2)).toFixed(2);
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
 
-    var BMI = document.createElement('p');
-    BMI.innerText = BMI_VALUE;
+        error_message = document.createElement('p');
+        error_message.innerText = "Please enter your weight and height";
 
-    result_container.appendChild(BMI);
+        result_container.appendChild(error_message);
+    } else {
+
+        var BMI_VALUE = (weight / (height**2)).toFixed(2);
+
+        var BMI = document.createElement('p');
+        BMI.innerText = 'Your BMI is: ' + BMI_VALUE;
+
+        result_container.appendChild(BMI);
+    }
 }
 
 function BMI_IMPERIAL() {
 
-    var weight = parseFloat(document.getElementById('INPUT_WEIGHT').value);
-    var height = parseInt(document.getElementById('INPUT_HEIGHT').value);
-    var result_container = document.getElementById('result');
+    var weight = parseFloat(document.getElementById('INPUT_WEIGHT_IMPERIAL').value);
+    var height = parseFloat(document.getElementById('INPUT_HEIGHT_IMPERIAL').value);
+    var result_container = document.getElementById('RESULT_IMPERIAL');
+    result_container.innerHTML = '';
 
-    var BMI_VALUE = ((weight / (height**2)) * 703).toFixed(2);
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
 
-    console.log(BMI_VALUE);
+        error_message = document.createElement('p');
+        error_message.innerText = "Please enter your weight and height";
+        
+        result_container.appendChild(error_message);
+    } else {
+    
+        var BMI_VALUE = ((weight / (height ** 2)) * 703).toFixed(2);
+    
+        var BMI = document.createElement('p');
+        BMI.innerText = 'Your BMI is: ' + BMI_VALUE;
+        
+        result_container.appendChild(BMI);
+    }    
+}
 
-    var BMI = document.createElement('p');
-    BMI.innerText = BMI_VALUE;
+function CHANGE_UNITS() {
+    
+    const METRIC_CONTAINER = document.getElementById('METRIC_SYSTEM');
+    const IMPERIAL_CONTAINER = document.getElementById('IMPERIAL_SYSTEM');
+    const CHANGE_UNITS_BTN = document.getElementById('CHANGE_UNITS_BTN');
+    var RESULT_METRIC = document.getElementById('RESULT_METRIC');
+    var RESULT_IMPERIAL = document.getElementById('RESULT_IMPERIAL');
+    var INPUT_WEIGHT_METRIC = document.getElementById('INPUT_WEIGHT_METRIC');
+    var INPUT_HEIGHT_METRIC = document.getElementById('INPUT_HEIGHT_METRIC');
+    var INPUT_WEIGHT_IMPERIAL = document.getElementById('INPUT_WEIGHT_IMPERIAL');
+    var INPUT_HEIGHT_IMPERIAL = document.getElementById('INPUT_HEIGHT_IMPERIAL');
 
-    result_container.appendChild(BMI);
+
+    if (METRIC_CONTAINER.style.display === 'none') {
+
+        INPUT_WEIGHT_METRIC.value = '';
+        INPUT_HEIGHT_METRIC.value = '';
+        INPUT_WEIGHT_IMPERIAL.value = '';
+        INPUT_HEIGHT_IMPERIAL.value = '';
+        METRIC_CONTAINER.style.display = 'flex';
+        IMPERIAL_CONTAINER.style.display = 'none';
+        CHANGE_UNITS_BTN.innerText = 'To imperial';
+        RESULT_METRIC.innerHTML = '';
+    } else {
+
+        INPUT_WEIGHT_METRIC.value = '';
+        INPUT_HEIGHT_METRIC.value = '';
+        INPUT_WEIGHT_IMPERIAL.value = '';
+        INPUT_HEIGHT_IMPERIAL.value = '';
+        METRIC_CONTAINER.style.display = 'none';
+        IMPERIAL_CONTAINER.style.display = 'flex';
+        CHANGE_UNITS_BTN.innerText = 'To metric';
+        RESULT_IMPERIAL.innerHTML = '';
+    }
 }
